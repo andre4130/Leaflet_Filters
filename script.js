2ef32f689ae671896edd2dcb6563d8b616cbcd03
Andre de Mendonça Maia <60257960+andre4130@users.noreply.github.com>
2020-05-07 11:50:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,14 +31,16 @@ function onMapLoad() {
 				html: kind_food[j]
 			}).appendTo('#kind_food_selector select');
 		}
+		markers.clearLayers();
+		render_to_map(data_markers, "All");
 		console.log($( "#kind_food_selector option:selected").text());
-		render_to_map(data_markers, 'all');
 
 		$('#kind_food_selector').on('change', function() {
 			console.log($(this).find(":selected").text());
 			filter = $(this).find(":selected").text();
 			render_to_map(data_markers, filter);
 		});
+
 	})};
 
 	function render_to_map(data_markers,filter){
@@ -46,15 +48,16 @@ function onMapLoad() {
 		if (filter == "All") {
 			for (var i = 0; i < data_markers.length; i++) {
 				console.log(data_markers[i])
-				var marker_temp = L.marker(new L.LatLng(data_markers[i].lat,data_markers[i].lng)).bindPopup(data_markers[i].Name + '<br> Type of food: ' + data_markers[i].kind_food).openPopup();
+				var marker_temp = L.marker(new L.LatLng(data_markers[i].lat,data_markers[i].lng)).bindPopup(data_markers[i].name + '<br> Type of food: ' + data_markers[i].kind_food).openPopup();
 				markers.addLayer(marker_temp).addTo(map).openPopup();
-			}} else {
-				for (var i = 0; i < data_markers.length; i++) {
-					if (data_markers[i].kind_food.includes(filter)) {
-						console.log(data_markers[i])
-						var marker_temp = L.marker(new L.LatLng(data_markers[i].lat,data_markers[i].lng)).bindPopup(data_markers[i].Name + '<br> Type of food: ' + data_markers[i].kind_food).openPopup();
-						markers.addLayer(marker_temp).addTo(map).openPopup();
-					}
+			}
+		} else {
+			for (var i = 0; i < data_markers.length; i++) {
+				if (data_markers[i].kind_food.includes(filter)) {
+					console.log(data_markers[i])
+					var marker_temp = L.marker(new L.LatLng(data_markers[i].lat,data_markers[i].lng)).bindPopup(data_markers[i].name + '<br> Type of food: ' + data_markers[i].kind_food).openPopup();
+					markers.addLayer(marker_temp).addTo(map).openPopup();
 				}
-			};
+			}
 		};
+	};
